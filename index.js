88592575f7feeda5babadf91c57518ee8c23b8e2
WalterOkumu <okumu.oriaro@gmail.com>
2021-05-30 23:09:28 +0300

    server for react-native set up

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const houses = require('./routes/houses')
+
+const app = express()
+
+app.use(express.json())
+
+// Enables CORS
+const cors = require('cors')
+app.use(cors({ origin: true }))
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the House Listing API')
+})
+
+app.use('/api/houses', houses)
+
+require('dotenv').config()
+
+const port = process.env.PORT || 3000
+
+mongoose.connect('mongodb+srv://okumu:O9o5LXrsLYxRUkBC@cluster0.v5lde.mongodb.net/house_app?retryWrites=true&w=majority')
+  .then(result => {
+    app.listen(port, () => console.log(`Server is running on port ${port}`))
+  })
+  .catch(err => console.log(err))
---
+++ routes/houses.js
@@ -0,0 +1,111 @@
+const express = require('express')
+
+const { check, validationResult } = require('express-validator')
+
+const House = require('../models/House')
+
+const router = express.Router()
+
+const validate = [
+  check('title')
+    .isLength({ min: 3, max: 50 })
+    .withMessage('Title should be between 3 to 50 characters'),
+  check('description')
+    .isLength({ min: 10, max: 200 })
+    .withMessage('Description should be between 10 to 200 characters'),
+  check('address')
+    .isLength({ min: 10, max: 100 })
+    .withMessage('Address should be between 10 to 100 characters'),
+  check('price')
+    .isNumeric()
+    .withMessage('Price should be a number')
+]
+
+// /api/houses
+router.post('/', validate, (req, res) => {
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    return res.status(422).send({ errors: errors.array() })
+  }
+
+  const house = new House({
+    title: req.body.title,
+    address: req.body.address,
+    homeType: req.body.homeType,
+    description: req.body.description,
+    price: req.body.price,
+    image: req.body.image,
+    yearBuilt: req.body.yearBuilt
+  })
+
+  house.save()
+    .then(result => {
+      res.send({
+        message: 'House data created successfully',
+        data: result
+      })
+    })
+    .catch(err => console.log(err))
+})
+
+// /api/houses --> list all houses
+router.get('/', (req, res) => {
+  House.find()
+    .then(houses => {
+      res.send(houses)
+    })
+    .catch(err => console.log(err))
+})
+
+// api/houses/id --> get item by ID
+router.get('/:id', (req, res) => {
+  const houseId = req.params.id
+
+  House.findById(houseId)
+    .then(house => {
+      res.send(house)
+    })
+    .catch(err => console.log(err))
+})
+
+// /api/houses/id--> update house
+router.put('/:id', validate, (req, res) => {
+  const houseId = req.params.id
+
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    return res.status(422).send({ errors: errors.array() })
+  }
+
+  House.findById(houseId)
+    .then(house => {
+      house.title = req.body.title
+      house.address = req.body.address
+      house.description = req.body.description
+      house.homeType = req.body.homeType
+      house.image = req.body.image
+      house.price = req.body.price
+      house.yearBuilt = req.body.yearBuilt
+
+      return house.save()
+    })
+    .then(result => {
+      res.send(result)
+    })
+    .catch(err => console.log(err))
+})
+
+// /api/houses/id--> delete house item
+router.delete('/:id', (req, res) => {
+  const houseId = req.params.id
+
+  House.findByIdAndRemove(houseId)
+    .then(result => {
+      res.send(result)
+    })
+    .catch(err => console.log(err))
+})
+
+module.exports = router
