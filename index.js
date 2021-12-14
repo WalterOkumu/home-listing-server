f7b91bc697329873ac61c91fc16cd73bc8cee6d8
WalterOkumu <okumu.oriaro@gmail.com>
2021-12-14 16:09:24 +0300

    updated environmental variables

--- index.js
+++ index.js
@@ -21,7 +21,11 @@ require('dotenv').config()
 
 const port = process.env.PORT || 3000
 
-mongoose.connect(`${process.env.DATABASE}`, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(
+  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@gikmokocluster.tmqhl.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true, useUnifiedTopology: true
+  })
   .then(() => {
     app.listen(port, () => console.log(`Server is running on port ${port}`))
   })
